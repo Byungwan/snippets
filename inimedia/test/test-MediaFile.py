9e4270bd4899796354582220a2596f13c24a6583
Byungwan Jun <byungwan.jun@inisoft.co.kr>
2016-12-01 18:54:06 +0900

    Update test-MediaFile.py

--- inimedia/test/test-MediaFile.py
+++ inimedia/test/test-MediaFile.py
@@ -16,13 +16,17 @@ print("test-out: expected 0, actual " + str(init_sidx.has_iseg()))
 f = open('init-only.mp4', 'rb')
 init_chunk = f.read()
 f.close()
-test_out.write_iseg(init_chunk, 168, 10000000, 0)
+# test_out.write_iseg(init_chunk, 168, 10000000, 0)
+test_out.write_iseg(init_chunk, 200, 10000000, 0)
 print("test-out: expected 1, actual " + str(init_sidx.has_iseg()))
 
 # write seg
-f = open('0.m4s', 'rb')
-seg_chunk = f.read()
-f.close()
-test_out.write_seg(seg_chunk, 0, 37546667)
+i = 0
+for segfile in [("0.m4s", 37546667), ("37546667.m4s", 37546666)]:
+    f = open(segfile[0], 'rb')
+    seg_chunk = f.read()
+    f.close()
+    test_out.write_seg(seg_chunk, i, segfile[1])
+    i += 1
 
 test_out.close()
