153a2fae8a180c1baa292c922de01cd77f3102b8
Byungwan Jun <byungwan.jun@inisoft.co.kr>
2016-12-02 09:59:44 +0900

    Improve the result message

--- inimedia/test/test-MediaFile.py
+++ inimedia/test/test-MediaFile.py
@@ -1,32 +1,51 @@
 import inimedia
 
 init_only = inimedia.MediaFile('init-only.mp4')
-print("init-only: expected 0, actual " + str(init_only.has_iseg()))
+iseg = init_only.has_iseg()
+print("init-only: expected iseg 0, actual " + str(iseg), end="")
+if iseg == 0:
+    print(' ... OK')
+else:
+    print(' ... NG')
 init_only.close()
 
 init_sidx = inimedia.MediaFile('init-sidx.mp4')
-print("init-sidx: expected 1, actual " + str(init_sidx.has_iseg()))
+iseg = init_sidx.has_iseg()
+print("init-sidx: expected iseg 1, actual " + str(iseg), end="")
+if iseg == 1:
+    print(' ... OK')
+else:
+    print(' ... NG')
 init_sidx.close()
 
 test_out = inimedia.MediaFile('test-out.mp4')
 test_out.open()
-print("test-out: expected 0, actual " + str(init_sidx.has_iseg()))
 
 # write iseg
 f = open('init-only.mp4', 'rb')
 init_chunk = f.read()
 f.close()
-# test_out.write_iseg(init_chunk, 168, 10000000, 0)
-test_out.write_iseg(init_chunk, 200, 10000000, 0)
-print("test-out: expected 1, actual " + str(init_sidx.has_iseg()))
+test_out.write_iseg(init_chunk, 168, 10000000, 0)
+iseg = test_out.has_iseg()
+print("test_out: expected iseg 1, actual " + str(iseg), end="")
+if iseg == 1:
+    print(' ... OK')
+else:
+    print(' ... NG')
 
 # write seg
 i = 0
-for segfile in [("0.m4s", 37546667), ("37546667.m4s", 37546666)]:
+for segfile in [("0.m4s", 37546667, 2699), ("37546667.m4s", 37546666, 54561)]:
     f = open(segfile[0], 'rb')
     seg_chunk = f.read()
     f.close()
-    test_out.write_seg(seg_chunk, i, segfile[1])
+    off = test_out.write_seg(seg_chunk, i, segfile[1])
+    print("test-out: expected offset " + str(segfile[2])
+          + ", actual " + str(off), end="")
+    if segfile[2] == off:
+        print(' ... OK')
+    else:
+        print(' ... NG')
     i += 1
 
 test_out.close()
