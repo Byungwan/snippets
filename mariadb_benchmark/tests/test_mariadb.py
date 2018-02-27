812fc4f3f90d8b2494b9234fa64e110b970072c0
Jun, Byungwan <byungwan.jun@inisoft.co.kr>
2018-02-27 16:54:00 +0900

    Update mariadb_benchmark

--- mariadb_benchmark/tests/test_mariadb.py
+++ mariadb_benchmark/tests/test_mariadb.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 blah blah
-"""
-
-import warnings
+'''
 
 import pymysql
+import warnings
 
 
 def test_mariadb_transaction():
-    db = pymysql.connect("localhost",
-                         "mariadb_benchmark",  # user
-                         "mariadb_benchmark",  # password
-                         "mariadb_benchmark")  # database
-    cursor = db.cursor()
+    db = pymysql.connect('localhost',
+                         'mariadb_benchmark',  # user
+                         'mariadb_benchmark',  # password
+                         'mariadb_benchmark')  # database
+    cur = db.cursor()
 
     with warnings.catch_warnings():
         warnings.simplefilter('ignore')
-        cursor.execute("DROP TABLE IF EXISTS test_timeline")
+        cur.execute('DROP TABLE IF EXISTS test_timeline')
 
-    sql = '''CREATE TABLE test_timeline (
+    stmt_create = '''CREATE TABLE test_timeline (
     media_start BIGINT UNSIGNED PRIMARY KEY NOT NULL,
     media_duration BIGINT UNSIGNED DEFAULT 0,
     media_sequence INT UNSIGNED DEFAULT 0,
@@ -28,29 +27,26 @@ def test_mariadb_transaction():
     file_offset BIGINT UNSIGNED DEFAULT 0,
     file_data_size INT UNSIGNED DEFAULT 0,
 
-    key_id BINARY(16)
-)'''
-    cursor.execute(sql)
+    key_id BINARY(16))'''
+    cur.execute(stmt_create)
 
-    sql = """INSERT INTO test_timeline (media_start, file_path)
-             VALUES (123456789, '/stg/ch001/video_eng_3000000/123.mp4')"""
+    seg_info = (123456789, '/stg/ch001/video_eng_3000000/123.mp4')
+    stmt_insert = '''INSERT INTO test_timeline (media_start, file_path)
+    VALUES (%s, %s)'''
     try:
-        cursor.execute(sql)
+        cur.execute(stmt_insert, seg_info)
         db.commit()
     except:
         db.rollback()
 
-    sql = "SELECT media_start, file_path FROM test_timeline"
+    stmt_select = 'SELECT media_start, file_path FROM test_timeline'
     try:
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        for row in results:
-            media_start = row[0]
-            file_path = row[1]
+        cur.execute(stmt_select)
+        res = cur.fetchall()
     except:
         pass
 
-    assert media_start == 123456789
-    assert file_path == "/stg/ch001/video_eng_3000000/123.mp4"
+    assert len(res) == 1
+    assert res[0] == seg_info
 
     db.close()
